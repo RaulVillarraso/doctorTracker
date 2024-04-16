071fe51a178f9f17e38a05fd54851d16322cf51d
RaulVillarraso <raulvillarraso@gmail.com>
2024-04-16 11:56:36 +0100

    returning to path importing original

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from './node_modules/vite'
-import vue from './node_modules/@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
